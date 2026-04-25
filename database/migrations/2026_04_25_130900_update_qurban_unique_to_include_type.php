e254a91692849a6ee5178009d76ae29c96596e98
Dony Ariesta <donyariesta.rin@gmail.com>
2026-04-25 22:32:22 +0700

    skip unique drop

--- database/migrations/2026_04_25_130900_update_qurban_unique_to_include_type.php
+++ database/migrations/2026_04_25_130900_update_qurban_unique_to_include_type.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('qurbans', function (Blueprint $table) {
-            $table->dropUnique('qurbans_event_id_qurban_number_unique');
+            // $table->dropUnique('qurbans_event_id_qurban_number_unique');
             $table->unique(['event_id', 'qurban_number', 'qurban_type'], 'qurbans_event_number_type_unique');
         });
     }
